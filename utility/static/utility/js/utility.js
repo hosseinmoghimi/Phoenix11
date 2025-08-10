b42cd80bb435023155ef355152ca346fb4916bbd
hossein.moghimi.ce <hossein.moghimi.ce@gmail.com>
2025-08-11 02:31:53 +0330

    card for project

--- utility/static/utility/js/utility.js
+++ utility/static/utility/js/utility.js
@@ -13,6 +13,18 @@ var to_price = function (x, currency) {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + ' ' + currency
 }
 
+var to_price_colored = function (x, currency) {
+    let color='muted'
+    if (x == null)
+        return ""
+    if(x>0)color='success'
+    if(x<0)color='danger'
+    if (typeof currency === 'undefined')
+        currency = ''
+    let ssss= x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + ' ' + currency
+    return `<span class="text-${color}">${ssss}</span>`
+}
+
 
 
 var showNotification = function (from, align, icon, bgcolor, html, rtl = true,farsi=false) {
