b353a77e0f26d32468877b9132367bfea74cc173
hossein.moghimi.ce <hossein.moghimi.ce@gmail.com>
2026-01-20 16:02:02 +0330

    ....

--- accounting/urls.py
+++ accounting/urls.py
@@ -135,7 +135,7 @@ urlpatterns = [
     path("make-financial-event-draft/",login_required(views.MakeFinancialEventDraftView.as_view()),name="make_financial_event_draft"),
       
     path("financial-events/",login_required(views.FinancialEventsView.as_view()),name="financial_events"),
-    path("new-financial-events/",login_required(views.NewFinancialEventView.as_view()),name="new_financial_event"),
+    path("new-financial-event/",login_required(views.NewFinancialEventView.as_view()),name="new_financial_event"),
     
     path("financial-event/<int:pk>/",login_required(views.FinancialEventView.as_view()),name="financialevent"),
     path("add-financial-event/",login_required(apis.AddFinancialEventApi.as_view()),name="add_financial_event"),
